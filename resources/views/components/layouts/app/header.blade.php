10ae9b6d5a52732c6d97256efee8229799f7b751
Jon Morby <jon@morby.org>
2025-03-16 10:55:52 +0000

    backup menu items added to mobile menu

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -67,6 +67,7 @@
 
                 <flux:menu>
                     <flux:menu.radio.group>
+
                         <div class="p-0 text-sm font-normal">
                             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                 <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-sm">
@@ -123,7 +124,9 @@
                     </flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
-
+                        <div>
+                            @livewire('task-backup-manager')
+                        </div>
             <flux:spacer />
 
             <flux:navlist variant="outline">
