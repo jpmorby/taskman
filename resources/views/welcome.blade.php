e49d09e33899d45ea3d8532c41df462e947cfdbf
Jon Morby <jon@morby.org>
2025-03-17 09:35:31 +0000

    version bumps and fork me on github ribbon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,19 @@
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://unpkg.com/@heroicons/vue@1.0.6/outline.js"></script>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css" />
+    <style>
+        .github-fork-ribbon:before {
+            background-color: #2cbe4e;
+        }
+    </style>
     </head>
     
     <body
         class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        <a class="github-fork-ribbon" href="https://github.com/jpmorby/taskman/" data-ribbon="Fork me on GitHub"
+            title="Fork me on GitHub">Fork me on GitHub</a>
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
