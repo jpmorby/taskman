704287d41b7f8ff3bee7cc9c582ed90481f7446c
Jon Morby <jon@morby.org>
2025-03-22 08:59:08 +0000

    no csrf on callbacks

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append([LanguageMiddleware::class]);
-
+        $middleware->validateCsrfTokens(except: [
+            'login/*/callback'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
