4e1bf5c02a8ac0f089aae5917bdeee89234e4e49
Jon Morby <jon@morby.org>
2025-03-22 09:16:26 +0000

    tidy up spacing

---
+++ resources/views/flux/icon/apple.blade.php
@@ -0,0 +1,42 @@
+{{-- Credit: Lucide (https://lucide.dev) --}}
+
+@props([
+    'variant' => 'outline',
+])
+
+@php
+if ($variant === 'solid') {
+    throw new \Exception('The "solid" variant is not supported in Lucide.');
+}
+
+$classes = Flux::classes('shrink-0')
+    ->add(match($variant) {
+        'outline' => '[:where(&)]:size-6',
+        'solid' => '[:where(&)]:size-6',
+        'mini' => '[:where(&)]:size-5',
+        'micro' => '[:where(&)]:size-4',
+    });
+
+$strokeWidth = match ($variant) {
+    'outline' => 2,
+    'mini' => 2.25,
+    'micro' => 2.5,
+};
+@endphp
+
+<svg
+    {{ $attributes->class($classes) }}
+    data-flux-icon
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="{{ $strokeWidth }}"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    aria-hidden="true"
+    data-slot="icon"
+>
+  <path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z" />
+  <path d="M10 2c1 .5 2 2 2 5" />
+</svg>
--- resources/views/livewire/auth/sso-buttons.blade.php
+++ resources/views/livewire/auth/sso-buttons.blade.php
@@ -1,21 +1,29 @@
 <div class="form-group">
     <div class="col-md-6 col-md-offset-4 text-center">
         @if (Route::has('login.socialite'))
-            <flux:table>
+            <flux:table class="gap-6 overflow-hidden">
                 <flux:table.rows>
                     <flux:table.row>
                         <flux:table.cell>
-                            <flux:button href="/login/github" icon="github" variant="outline" class="btn btn-github"> Github
+                            <flux:button size="xs" href="/login/github" icon="github" variant="outline" class="btn btn-github">
+                                Github
                             </flux:button>
-                        </flux:table.cell>
-                        <flux:table.cell>
-                            <flux:button href="/login/google" icon="google" variant="outline" class="btn btn-google"> Google
-                            </flux:button>
-                        </flux:table.cell>
-                        <flux:table.cell>
-                            <flux:button href="/login/discord" icon="discord" variant="outline" class="btn btn-discord">
-                                Discord</flux:button>
-                        </flux:table.cell>
+                            </flux:table.cell>
+                            <flux:table.cell>
+                                <flux:button size="xs" href="/login/google" icon="google" variant="outline" class="btn btn-google">
+                                    Google
+                                </flux:button>
+                                </flux:table.cell>
+                                <flux:table.cell>
+                                <flux:button size="xs" href="/login/discord" icon="discord" variant="outline" class="btn btn-discord">
+                                    Discord
+                                </flux:button>
+                                </flux:table.cell>
+                                {{-- <flux:table.cell>
+                                    <flux:button size="xs" href="/login/apple" icon="apple" variant="outline" class="btn btn-apple">
+                                        Apple
+                                    </flux:button>
+                                </flux:table.cell> --}}
                         </flux:row>
                         </flux:rows>
             </flux:table>
