3e70b263cfdef172e059228e7af5ba5f89ecff33
Jon Morby <jon@morby.org>
2025-03-24 11:29:06 +0000

    fix: 500 error

--- resources/views/livewire/search-box.blade.php
+++ resources/views/livewire/search-box.blade.php
@@ -1,11 +1,11 @@
-<div id="search" class="px-5 py-5 mb-10" x-data="{}" x-init="$el.querySelector('input').id = 'searchInput'; 
+<div id="search" x-data="{}" x-init="$el.querySelector('input').id = 'searchInput'; 
             window.addEventListener('keydown', (e) => {
                 if ((e.metaKey || e.ctrlKey) && e.key === 'k') {
                     e.preventDefault();
                     document.getElementById('searchInput').focus();
                 }
             })">
-    <flux:input id="searchInput" label="{{ __('Search') }}: " wire:keydown.meta.k="search" kbd="⌘K" icon="magnifying-glass"
+    <flux:input id="searchInput" label="{{ __('Search') }}: " icon="magnifying-glass" kbd="⌘K"
         wire:model.debounce.500ms.live="needle" />
-
+{{-- wire:keydown.meta.k="search" --}}
 </div>
