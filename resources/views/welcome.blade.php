bb0ba2466154bd3962102fb4f05fc056b79da28e
Jon Morby <jon@morby.org>
2026-02-05 05:55:23 +0000

    added analytics

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,10 +54,10 @@
             class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 @php
-                    use App\Models\User;
-                    use App\Models\Task;
-                    $userCount = User::count();
-                    $taskCount = Task::count();
+use App\Models\User;
+use App\Models\Task;
+$userCount = User::count();
+$taskCount = Task::count();
                 @endphp
                 <flux:table>
                     <flux:table.rows>
@@ -88,6 +88,7 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+        <x-analytics />
     </body>
 
 </html>
